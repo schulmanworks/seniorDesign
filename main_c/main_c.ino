dd762ff174e2c03822a91ffc35711dd9313951e9
Ryan Schulman <schulmanworks@gmail.com>
2017-11-03 12:14:27 -0500

    working on statemachine, interrupts work

--- main_c/main_c.ino
+++ main_c/main_c.ino
@@ -4,19 +4,27 @@
 
 #include <EEPROM.h>
 
+// #include <Serial.h>
+
 
 #define COUNT_ADDR 0
 #define EEPROM_CANARY 10
+#define OPEN = 1
+#define CLOSE = 0
 
-const int pushButtonPin = 11;     // the number of the pushbutton pin
-const int closeButtonPin = 12;
-const int ledPin =  13;      // the number of the LED pin
+
+enum State {Initial=0, Neutral=1, Closed = 2, Opened = 3};
+
+
+const int pushButtonPin = 2;     // the number of the pushbutton pin
+const int closeButtonPin = 3;
+// const int ledPin =  13;      // the number of the LED pin
 
 // variables will change:
 volatile int pushButtonState = 0;         // variable for reading the pushbutton status
 volatile int closeButtonState = 0;
 volatile unsigned int count = 0;
-
+enum State curState = Initial;
 
 
 void setup() {
@@ -24,12 +32,15 @@ void setup() {
   uint8_t canary;
   unsigned int tmp_count;
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  // pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
+  Serial.begin(9600);
+
+  pinMode(closeButtonPin, INPUT);
   pinMode(pushButtonPin, INPUT);
   // Attach an interrupt to the ISR vector
   attachInterrupt(0, pushButton_ISR, CHANGE);
-  attachInterrupt(0, closeButton_ISR, CHANGE);
+  attachInterrupt(1, closeButton_ISR, CHANGE);
 
 
   //If the EEPROM has been initialized, grab the value
@@ -52,14 +63,29 @@ void setup() {
 }
 
 void loop() {
-  // Nothing here!
+  // Nothing here!]
+  curState = Neutral;
+  int oldState = 0;
+  while(1){
+  }
 }
 
 void closeButton_ISR() {
- closeButtonState = digitalRead(pushButtonPin);
+ closeButtonState = digitalRead(closeButtonPin);
+ if (closeButtonState) {
+
+ }
+ else {
+
+ }
+ // Serial.print("cl\n");
 }
 
 void pushButton_ISR() {
   pushButtonState = digitalRead(pushButtonPin);
+  switch (curState) {
+    case
 
+  }
+  // Serial.print("op\n");
 }
